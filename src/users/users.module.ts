88a122deaf86c2aead49d2aaa8f0a2d7860dcfc0
p.resende <paulo.resende@rbmweb.com.br>
2023-06-30 17:40:05 -0300

    feat: auth all users routes

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    }
+  ],
   imports: [PrismaModule],
   exports: [UsersService],
 })
